883f170dd94064bf294b28003ffd2cbb4507dc57
Jasmine Rudolph <jazcrudolph@gmail.com>
2021-05-15 20:09:15 -0600

    adding functions to cycle through quiz questions

--- script.js
+++ script.js
@@ -1,32 +1,86 @@
 const startBtn = document.getElementById('start-btn')
+const nextBtn = document.getElementById('next-btn')
+
 const questConEl = document.getElementById('container-quest')
 const questEl = document.getElementById('question')
 const answersEl = document.getElementById('answer-btns')
+// let currentQuesIndex = 0;
+
+
 
 startBtn.addEventListener('click', initQuiz)
+nextBtn.addEventListener('click', () => {
+    currentQuesIndex++
+    setNextQues()
+})
 
 function initQuiz() {
-    console.log('started');
     //// Will HIDE start button and SHOW hidden elements in container
     startBtn.classList.add('hidden');
     currentQuesIndex = 0;
     questConEl.classList.remove('hidden');
-    setNextQues()
+    setNextQues();
 }
 
-function setNextQues () {
-    showQuest(currentQuesIndex++);
+function setNextQues() {
+    resetBoard();
+    showQuest(quizQuestion[currentQuesIndex]);
 }
 
 function showQuest(question) {
-        questEl.innerText = question.question
+        questEl.innerText = question.question;
+        question.answers.forEach( answer => {
+            const button = document.createElement('button')
+            button.innerText = answer.text
+            button.classList.add('btn')
+            if (answer.correct) {
+                button.dataset.correct = answer.correct
+            }
+            button.addEventListener('click', userSelect)
+            answersEl.appendChild(button);
+
+        })
 }
 
-function userSelect () {
+function resetBoard () {
+    nextBtn.classList.add('hidden')
 
+    /// "If there's a child inside the element, remove it"
+    while (answersEl.firstChild) {
+        answersEl.removeChild(answersEl.firstChild);
+    }
 }
 
-let question = [
+function userSelect (e) {
+    const btnSelected = e.target 
+    const correct = btnSelected.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answersEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if(quizQuestion.length > currentQuesIndex + 1){
+        nextBtn.classList.remove('hidden')
+    } else {
+        startBtn.innerText = 'Restart'
+        startBtn.classList.remove('hidden')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatus(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    } 
+}
+
+function clearStatus (element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
+}
+
+const quizQuestion = [
     {
     question: "Which is NOT a fruit?",
     answers: [
@@ -35,5 +89,36 @@ let question = [
         {text: 'Potato', correct: true },
         {text: 'Pineapple', correct: false }
         ]
+    },
+
+    {
+    question: "Which country is in South America?",
+    answers: [
+        {text: "USA", correct: false},
+        {text: "Chile", correct: true},
+        {text: "Mexico", correct: false},
+        {text: "China", correct: false}
+        ]
+    },
+
+    {
+    question: "What color is the sky?",
+    answers: [
+        {text: "Magenta", correct: false},
+        {text: "Purple", correct: false},
+        {text: "Red", correct: false},
+        {text: "Blue", correct: true}
+        ]
+    },
+
+    {
+    question: "How many months have 28 days?",
+    answers: [
+        {text: " 1(Duhh)", correct: false},
+        {text: "3", correct: false},
+        {text: "6", correct: false},
+        {text: "12", correct: true}
+        ]
     }
+
 ]
